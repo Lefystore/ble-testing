b4b807be46237671014be6ef8f2f395da31be12a
xsmurphy <drahgster@gmail.com>
2017-09-28 23:50:12 -0400

    uyt

--- www/js/index.js
+++ www/js/index.js
@@ -1,92 +1,3 @@
-/*
- * Licensed to the Apache Software Foundation (ASF) under one
- * or more contributor license agreements.  See the NOTICE file
- * distributed with this work for additional information
- * regarding copyright ownership.  The ASF licenses this file
- * to you under the Apache License, Version 2.0 (the
- * "License"); you may not use this file except in compliance
- * with the License.  You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing,
- * software distributed under the License is distributed on an
- * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
- * KIND, either express or implied.  See the License for the
- * specific language governing permissions and limitations
- * under the License.
- *
-var app = {
-    // Application Constructor
-    initialize: function() {
-        this.bindEvents();
-    },
-    // Bind Event Listeners
-    //
-    // Bind any events that are required on startup. Common events are:
-    // 'load', 'deviceready', 'offline', and 'online'.
-    bindEvents: function() {
-        document.addEventListener('deviceready', this.onDeviceReady, false);
-    },
-    // deviceready Event Handler
-    //
-    // The scope of 'this' is the event. In order to call the 'receivedEvent'
-    // function, we must explicitly call 'app.receivedEvent(...);'
-    onDeviceReady: function() {
-        console.log('device ready');
-        alert('device ready');
-
-
-        var onSuccess = function(position) {
-        alert('Latitude: '          + position.coords.latitude          + '\n' +
-              'Longitude: '         + position.coords.longitude         + '\n' +
-              'Altitude: '          + position.coords.altitude          + '\n' +
-              'Accuracy: '          + position.coords.accuracy          + '\n' +
-              'Altitude Accuracy: ' + position.coords.altitudeAccuracy  + '\n' +
-              'Heading: '           + position.coords.heading           + '\n' +
-              'Speed: '             + position.coords.speed             + '\n' +
-              'Timestamp: '         + position.timestamp                + '\n');
-    };
- 
-    // onError Callback receives a PositionError object 
-    // 
-    function onError(error) {
-        alert('code: '    + error.code    + '\n' +
-              'message: ' + error.message + '\n');
-    }
- 
-    navigator.geolocation.getCurrentPosition(onSuccess, onError);
-
-
-
-        if(!ble){
-            alert('no plugin ble');
-            var ble = {};
-            return;
-        }
-        ble.startScan([], function(device) {
-            alert(device);
-            var ul = '';
-            $.each(device,function(k,v){
-                ul = '<li>'+v+'</li>';
-            });
-            $('#blelist').html(ul);
-            console.log(JSON.stringify(device));
-        }, function(){
-            alert('scan fgailed');
-        });
-
-        setTimeout(ble.stopScan,
-            5000,
-            function() { alert("Scan complete"); },
-            function() { alert("stopScan failed"); }
-        );
-    }
-    
-};
-
-*/
-
 'use strict';
 
 var battery = {
@@ -176,8 +87,7 @@ var app = {
     },
     ascii_to_hexa: function(str){
         var arr1 = [];
-        for (var n = 0, l = str.length; n < l; n ++) 
-         {
+        for (var n = 0, l = str.length; n < l; n ++) {
             var hex = Number(str.charCodeAt(n)).toString(16);
             arr1.push(hex);
          }
